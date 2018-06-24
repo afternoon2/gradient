c0b2de59e3fabd549f4dab50c6bd672ab9c56f34
Jakub Antolak <poprostuantolak@gmail.com>
2018-06-24 18:29:23 +0200

    Make Validator a universal component

--- src/Validator.ts
+++ src/Validator.ts
@@ -1,10 +1,8 @@
-import * as chroma from 'chroma-js'
-
 export type OptionsInterpolation = 'linear' | 'bezier'
 
 export type OptionsMode = 'none' | 'lrgb' | 'lch' | 'hsv' | 'lab'
 
-export type ValidatorOptions = {
+export type BaseOptions = {
     interpolation: OptionsInterpolation,
     samples: number,
     mode: OptionsMode,
@@ -12,98 +10,95 @@ export type ValidatorOptions = {
 }
 
 export default class Validator {
-    private errorMessage: string = `
-        Wrong input format. Following string color types are accepted:
-        - hex
-        - rgba like:
-        'rgba(255, 255, 255, 0.25)' or
-        'rgba(255, 255, 255, .25)'
-    `
-    private input: string[]
-    private config: ValidatorOptions
+    private invalidColorFormatMessage: string
+    private invalidConfigMessage: string
+    private mixedArrayMessage: string
     private hex: RegExp
     private rgba: RegExp
 
-    constructor(
-        private colors: string[],
-        private options: ValidatorOptions
-    ) {
-        this.input = colors
-        this.config = options
+    constructor() {
         this.hex = /^#(?:[0-9a-fA-F]{3}){1,2}$/
         this.rgba = /rgba\(([0-9]+,\s)([0-9]+,\s)([0-9]+,\s)([0-9]?\.?[0-9]\))/
-        this.validateInput()
-        this.validateOptions()
+        this.invalidColorFormatMessage = `
+            Wrong input format. Following string color types are accepted:
+            - hex
+            - rgba like:
+            'rgba(255, 255, 255, 0.25)' or
+            'rgba(255, 255, 255, .25)'
+        `
+        this.mixedArrayMessage = `
+            Colors array contains strings of both rgba and hex strings.
+            Please provide rgba strings only.
+        `
+        this.invalidConfigMessage = 'Invalid input object provided'
     }
 
-    public get inputs(): string[] {
-        return this.input
+    public validateColors(colors: string[]) {
+        if (this.findInvalidString(colors)) {
+            throw new Error(this.invalidColorFormatMessage)
+        }
+        if (this.isMixedArray(colors)) {
+            throw new Error(this.mixedArrayMessage)
+        }
     }
 
-    public get configuration(): ValidatorOptions {
-        return this.config
+    public validateOptions(options: any) {
+        const optsType: string = this.getOptionsType(options)
+        switch (optsType) {
+            case 'base':
+                this.validateBaseOptions(options)
+                break
+            default:
+                return
+        }
     }
 
-    private validateOptions() {
+    private getOptionsType(options: any): string {
+        if (options.interpolation) {
+            return 'base'
+        } else if (options.cssGradientType) {
+            return 'css'
+        }
+    }
+
+    private validateBaseOptions(options: BaseOptions) {
         if (
             (
-                (<ValidatorOptions> this.config).interpolation !== 'linear' &&
-                (<ValidatorOptions> this.config).interpolation !== 'bezier'
+                options.interpolation !== 'linear' &&
+                options.interpolation !== 'bezier'
             ) ||
             (
-                (<ValidatorOptions> this.config).mode !== 'none' &&
-                (<ValidatorOptions> this.config).mode !== 'lch' &&
-                (<ValidatorOptions> this.config).mode !== 'lab' &&
-                (<ValidatorOptions> this.config).mode !== 'lrgb' &&
-                (<ValidatorOptions> this.config).mode !== 'hsv'
+                options.mode !== 'none' &&
+                options.mode !== 'lch' &&
+                options.mode !== 'lab' &&
+                options.mode !== 'lrgb' &&
+                options.mode !== 'hsv'
             ) ||
-            typeof this.config.samples !== 'number' ||
-            typeof this.config.lightnessCorrection !== 'boolean'
+            typeof options.samples !== 'number' ||
+            typeof options.lightnessCorrection !== 'boolean'
         ) {
-            throw new Error('Invalid input object provided')
-        }
-        if (
-            this.config.interpolation === 'bezier' &&
-            this.config.mode !== 'none'
-        ) {
-            this.config.mode = 'none'
+            throw new Error(this.invalidConfigMessage)
         }
     }
 
-    private validateInput() {
-        if (this.findInvalidString()) {
-            throw new Error(this.errorMessage)
-        }
-        this.checkIfHex()
-    }
-
-    private findInvalidString(): boolean {
-        const invalidString: string = this.input
+    private findInvalidString(colors: string[]): boolean {
+        const invalidString: string = colors
             .find(color => !this.hex.test(color) && !this.rgba.test(color))
         return invalidString !== undefined ? true : false
     }
 
-    private checkIfHex() {
-        const hexIndexes: number[] = this.findHexStrings()
-        if (hexIndexes.length > 0) {
-            hexIndexes.forEach(index => {
-                this.input[index] = this.hexToRgba(this.input[index])
-            })
-        }
+    private isMixedArray(colors: string[]): boolean {
+        const hexIndex: number = this.find(colors, 'hex')
+        const rgbaIndex: number = this.find(colors, 'rgba')
+        return hexIndex > -1 && rgbaIndex > -1 ?
+            true : false
     }
 
-    private findHexStrings(): number[] {
-        const indexes: number[] = []
-        this.input.forEach(color => {
-            if (this.hex.test(color)) {
-                indexes.push(this.input.indexOf(color))
-            }
-        })
-        return indexes
-    }
-
-    private hexToRgba(color: string): string {
-        const colorArr: number[] = chroma(color).rgba()
-        return `rgba(${colorArr[0]}, ${colorArr[1]}, ${colorArr[2]}, ${colorArr[3]})`
+    private find(
+        colors: string[],
+        format: 'hex' | 'rgba'
+    ): number {
+        return colors
+            .findIndex(color => this[format].test(color))
     }
 }
--- tests/Validator.test.ts
+++ tests/Validator.test.ts
@@ -1,103 +1,62 @@
 import Validator, {
-    ValidatorOptions
+    BaseOptions
 } from '../src/Validator'
 
-const hexInputs: string[] = [
-    '#ffcc00',
-    '#d39f00'
+const fakeColors: string[] = [
+    'I am not a color!',
+    'Me too!'
 ]
 
-const mixedInputs: string[] = [
-    'rgba(0, 244, 44, 0.3)',
-    '#d39f00'
+const mixedColors: string[] = [
+    'rgba(0, 0, 0, 0.4)',
+    '#fcf01f'
 ]
 
-const rgbaInputs: string[] = [
-    'rgba(0, 244, 44, 0.3)',
-    'rgba(0, 244, 44, 0.3)'
-]
-
-const fakeInputs: string[] = [
-    'I am not hex color',
-    'I am not rgba color'
-]
-
-const opts: ValidatorOptions = {
+const options: BaseOptions = {
     interpolation: 'linear',
     samples: 10,
     mode: 'none',
-    lightnessCorrection: true
-}
-
-const optsToCorrect: ValidatorOptions = {
-    interpolation: 'bezier',
-    samples: 10,
-    mode: 'lab',
     lightnessCorrection: false
 }
 
-const optsCorrected: ValidatorOptions = {
-    interpolation: 'bezier',
-    samples: 10,
-    mode: 'none',
-    lightnessCorrection: false
+const fakeOptions: any = {
+    interpolation: 'No interpolation provided',
+    samples: false,
+    mode: 'rgb',
+    lightnessCorrection: 1
 }
 
-const fakeOpts: ValidatorOptions = {
-    interpolation: 'I am not the interpolation, lol',
-    samples: 10,
-    mode: 'I am not a mode, huh',
-    lightnessCorrection: false
-}
-
-const errorMessagePart: string = 'Wrong input format.'
-
+// Test colors
 test(
-    'If input colors validation throws error on wrong string type',
+    'If validator throws error on invalid colors array',
     () => {
-        const safeValidate: any = (): void => {
-            const validator: Validator = new Validator(fakeInputs, opts)
+        const validator: Validator = new Validator()
+        const safeValidation = (): void => {
+            validator.validateColors(fakeColors)
         }
-        expect(safeValidate).toThrowError(errorMessagePart)
+        expect(safeValidation).toThrowError('Wrong input format')
     }
 )
 
 test(
-    'If input colors validation changes hex values in mixed arrays into rgba strings',
+    'If validator throws error on mixed color types array',
     () => {
-        const hex: RegExp = /^#(?:[0-9a-fA-F]{3}){1,2}$/
-        const validator: Validator = new Validator(mixedInputs, opts)
-        expect(validator.inputs.findIndex(color => hex.test(color))).toBe(-1)
-    }
-)
-
-test(
-    'If input colors validation converts all hex values into rgba strings',
-    () => {
-        const rgba: RegExp = /rgba\(([0-9]+,\s)([0-9]+,\s)([0-9]+,\s)([0-9]?\.?[0-9]\))/
-        const validator: Validator = new Validator(hexInputs, opts)
-        const firstIsValid: boolean = rgba.test(validator.inputs[0])
-        const secondIsValid: boolean = rgba.test(validator.inputs[1])
-
-        expect(firstIsValid).toEqual(true)
-        expect(secondIsValid).toEqual(true)
-    }
-)
-
-test(
-    'If options validation makes a valid correction of the color mode',
-    () => {
-        const validator: Validator = new Validator(rgbaInputs, optsToCorrect)
-        expect(validator.configuration).toEqual(optsCorrected)
-    }
-)
-
-test(
-    'If options validation throws an error after receiving invalid options object',
-    () => {
-        const safe = (): void => {
-            const validator: Validator = new Validator(rgbaInputs, fakeOpts)
+        const validator: Validator = new Validator()
+        const safeValidation = (): void => {
+            validator.validateColors(mixedColors)
         }
-        expect(safe).toThrowError('Invalid input object')
+        expect(safeValidation).toThrowError('Colors array contains strings')
+    }
+)
+
+// Test base options
+test(
+    'If base options validation throws error on invalid base options object',
+    () => {
+        const validator = new Validator()
+        const safeValidation = (): void => {
+            validator.validateOptions(fakeOptions)
+        }
+        expect(safeValidation).toThrowError('Invalid input object')
     }
 )
